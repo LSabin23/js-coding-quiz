126180b22578b25c40503ef9b16fe58bdd086024
unknown <44147809+LSabin23@users.noreply.github.com>
2021-10-29 19:10:13 -0400

    created global question counter variable and made timeLeft global

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,9 @@ var submitFormEl = document.querySelector('#submit-score')
 var restartButtonEl = document.querySelector('#restart-btn')
 var saveScoresButtonEl = document.querySelector('#save-scores-btn')
 var highScoresEl = document.querySelector('#high-scores')
+var questionCounter = 0
+// set timeLeft to 75 if not testing
+var timeLeft = 15
 
 // add array of question objects with title and answer properties
 var quizArray = [
@@ -48,6 +51,11 @@ var quizArray = [
 ]
 
 var startQuiz = function () {
+  // set intro div display to hidden
+  introEl.classList.toggle('hidden')
+  // set questions div display to shown
+  questionEl.classList.toggle('hidden')
+  
   // display timer value to #timer element
   countdownTimer()
 
@@ -57,8 +65,6 @@ var startQuiz = function () {
 
 var countdownTimer = function () {
   // start timer from 75 seconds and countdown by 1000ms
-  var timeLeft = 15
-
   var timeInterval = setInterval(function () {
     if (timeLeft < 0) {
       clearInterval(timeInterval)
@@ -66,7 +72,10 @@ var countdownTimer = function () {
       submitScore()
     }
     // add condition to check if answer button clicked property name is not rightAnswer then subtract 10 from timeLeft
-    // else if {
+    // else if (CLICKED BUTTON ID IS NOT CORRECT-ANSWER) {
+      // timeLeft = timeLeft - 10
+      // questionCounter = questionCounter + 1
+      // askQuestion()
     // }
     else {
       // display timer value to timerEl.textContent
@@ -91,45 +100,44 @@ var submitScore = function () {
 }
 
 var askQuestion = function () {
-  // set intro div display to hidden
-  introEl.classList.toggle('hidden')
-  // set questions div display to shown
-  questionEl.classList.toggle('hidden')
-
   // set question div to first array object content
-  for (var i = 0; i < quizArray.length; i++) {
-    // create new h2 element for question text
-    var questionTextEl = document.createElement('h2')
-    // set question text to index object's title property content
-    questionTextEl.textContent = quizArray[i].title
-    // append question text element to question el container for visibility
-    questionEl.appendChild(questionTextEl)
-    // create button element for each answer property on index object
-    var answerChoice1 = document.createElement('button')
-    answerChoice1.textContent = quizArray[i].answer1
-    questionEl.appendChild(answerChoice1)
+  // create new h2 element for question text
+  var questionTextEl = document.createElement('h2')
+  // set question text to index object's title property content
+  questionTextEl.textContent = quizArray[questionCounter].title
+  // append question text element to question el container for visibility
+  questionEl.appendChild(questionTextEl)
+  // create button element for each answer property on index object
+  var answerChoice1 = document.createElement('button')
+  answerChoice1.textContent = quizArray[questionCounter].answer1
+  questionEl.appendChild(answerChoice1)
 
-    var answerChoice2 = document.createElement('button')
-    answerChoice2.textContent = quizArray[i].answer2
-    questionEl.appendChild(answerChoice2)
+  var answerChoice2 = document.createElement('button')
+  answerChoice2.textContent = quizArray[questionCounter].answer2
+  questionEl.appendChild(answerChoice2)
 
-    var answerChoiceRight = document.createElement('button')
-    answerChoiceRight.setAttribute('id', 'correct-answer')
-    answerChoiceRight.textContent = quizArray[i].answerRight
-    questionEl.appendChild(answerChoiceRight)
+  var answerChoiceRight = document.createElement('button')
+  answerChoiceRight.setAttribute('id', 'correct-answer')
+  answerChoiceRight.textContent = quizArray[questionCounter].answerRight
+  questionEl.appendChild(answerChoiceRight)
 
-    var answerChoice4 = document.createElement('button')
-    answerChoice4.textContent = quizArray[i].answer4
-    questionEl.appendChild(answerChoice4)
+  var answerChoice4 = document.createElement('button')
+  answerChoice4.textContent = quizArray[questionCounter].answer4
+  questionEl.appendChild(answerChoice4)
 
-    // add event listener for click on a button with id correct-answer
-    questionEl.addEventListener('click', 'button', function (event) {
-      if (event.target.id !== 'correct-answer') {
-        // subtract 10 seconds from timer
-        // continue the for loop
-      }
-    })
-  }
+  // add event listener for click on a button with id correct-answer
+  questionEl.addEventListener('click', function (event) {
+    if (event.target.id !== 'correct-answer') {
+      // subtract 10 seconds from timer
+      timeLeft = timeLeft - 10
+      questionCounter = questionCounter + 1
+      askQuestion()
+    }
+    else {
+      questionCounter = questionCounter + 1
+      askQuestion()
+    }
+  })
 }
 
 var reloadPage = function () {
